7b5dca4e51e7e293de2ea493f9e0e70260777866
Slubateau1040 <elouan.taupinn@proton.me>
2026-09-17 15:58:26 +0200

    Add base template and start login setup

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'riddim',
+    'sample',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
---
+++ riddim/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RiddimConfig(AppConfig):
+    name = 'riddim'
